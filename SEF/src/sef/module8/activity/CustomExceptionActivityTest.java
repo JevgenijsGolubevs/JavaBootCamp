068f42ebe1309fb0b14a5e461b3af9baec365816
Jevgenijs Golubevs <jevgenijs.golubevss@gmail.com>
2021-06-03 12:38:01 +0300

    JevgenijsGolubevs

--- SEF/src/sef/module8/activity/CustomExceptionActivityTest.java
+++ SEF/src/sef/module8/activity/CustomExceptionActivityTest.java
@@ -3,32 +3,42 @@ package sef.module8.activity;
 //This program accepts a user name and checks user's validity
 //The user name is checked against an array of names.
 
+import java.rmi.ConnectIOException;
+
 public class CustomExceptionActivityTest {
-    public static void main (String args[]){
-    validateUser();
-    }
+
     //1 - Type main method and call validateUser() from it.
     //Call this method with different names to test it
+    public static void main(String args[]) throws CustomExceptionActivity {
+        validateUser("Mike");
+        validateUser("Joshua");
 
-
-    static void validateUser(){}
-            String name = "Joshua";
-    {
-        //array of names
-        String []validUsers={"John","Mike","Shanti","Stacie"};
-//flag stores 1 if a match is found else it should remain 0
-        int flag=0;
-        for(int i=0;i<4;i++)
-        {
-
-//2 - Write code to check if parameter name contains a value which is found in validUsers array and change flag's value accordingly
-
-        }
-//3 - check if flag is zero, throw CustomExceptionActivity Exception.
-//You may also have to declare this exception in the method call using throws.
-
-
-//4 - else if flag is one, print a message "Welcome to Payroll program".
 
     }
-}
+
+    static void validateUser(String name) throws CustomExceptionActivity {
+
+        //array of names
+        String[] validUsers = {"John", "Mike", "Shanti", "Stacie"};
+//flag stores 1 if a match is found else it should remain 0
+        int flag = 0;
+        for (int i = 0; i < 4; i++)
+            {
+//2 - Write code to check if parameter name contains a value which is found in validUsers array and change flag's value accordingly
+                if (name == validUsers[i]) {
+                    flag = 1;
+
+                }
+            }
+//3 - check if flag is zero, throw CustomExceptionActivity Exception.
+//You may also have to declare this exception in the method call using throws.
+            if (flag == 0) {
+                throw new CustomExceptionActivity();
+            }
+
+//4 - else if flag is one, print a message "Welcome to Payroll program".
+            else if (flag == 1) {
+                System.out.println("Welcome to Payroll program");
+            }
+        }
+    }
